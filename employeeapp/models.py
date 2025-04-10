e6bf8d8ee254b773843293134b449ee7a51e4625
Nilam <shiledaraa@rknec.edu>
2025-04-10 17:19:05 +0530

    final changes

--- employeeapp/models.py
+++ employeeapp/models.py
@@ -1,14 +1,27 @@
 from django.db import models
+from django.utils import timezone
 
 class Employee(models.Model):
-    name = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
-    bank_account_no = models.CharField(max_length=30)
-    gender = models.CharField(max_length=10)
-    address = models.TextField()
-    projects_done_in_year = models.IntegerField()
-    salary = models.FloatField()
-    term_in_company = models.IntegerField()
+    GENDER_OPTIONS = [
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Other', 'Other'),
+        ('Unspecified', 'Unspecified'),
+    ]
+
+    given_name = models.CharField(max_length=100, blank=True, null=True)
+    family_name = models.CharField(max_length=100, blank=True, null=True)
+    work_email = models.EmailField(unique=True, null=True, blank=True)
+
+    mobile = models.CharField(max_length=15, blank=True, null=True)
+    sex = models.CharField(max_length=20, choices=GENDER_OPTIONS, default='Unspecified')
+    home_address = models.TextField(blank=True, null=True)
+    account_number = models.CharField(max_length=30, blank=True, null=True)
+    role = models.CharField(max_length=100, default='Employee')
+    base_pay = models.FloatField(default=0.0)
+    completed_projects = models.IntegerField(default=0)
+    years_employed = models.IntegerField(default=0, help_text="How long the person has worked here (in years)")
+    joined_on = models.DateField(default=timezone.now)
 
     def __str__(self):
-        return f"{self.name} {self.surname}"
+        return f"{self.given_name or ''} {self.family_name or ''}".strip()
